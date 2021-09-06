a96a778861d0facd73531aef3eab0cba90d1b00c
erosboysuper <erosboysuper@gmail.com>
2021-09-06 02:42:01 -0700

    first commit

---
+++ src/component/CellWrapper.tsx
@@ -0,0 +1,38 @@
+import React, { Component, CSSProperties } from 'react';
+import Row from '../model/row';
+
+
+export type CellWrapperProps = {
+  row: Row;
+  renderCell: (row: Row) => React.ReactNode;
+
+  grow?: number;
+  basis?: string; // <CSS size> | auto
+}
+
+export default class CellWrapper extends Component<CellWrapperProps, {}> {
+  static defaultProps = {
+    grow: 1,
+    basis: '0px',
+  };
+
+
+  render() {
+    const { row, renderCell, grow, basis } = this.props;
+
+    return (
+      <div className={`cp_tree-table_cell`}
+        style={{ ...STYLE_COLUMN, flexGrow: grow, flexBasis: basis }}>
+        { renderCell(row) }
+      </div>
+    );
+  }
+}
+
+
+const STYLE_COLUMN: CSSProperties = {
+  boxSizing: 'border-box',
+  position: 'relative',
+  overflow: 'hidden',
+  width: '100%',
+};
